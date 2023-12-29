f95f89f6003b7df2abadb0ed5d05f9e33cc16459
Sakibul Islam <120340281+SI2105@users.noreply.github.com>
2023-12-29 22:16:41 +0000

    DNode class created
    DNode will be the class used to create the Nodes for our DLinkedList containing: value, pointer to next node in the list and pointer to previous node in the list.

---
+++ data-structures/doubly-linked-list.py
@@ -0,0 +1,9 @@
+class DNode: #Doubly Linked List Node with the required variables.
+    def __init__(self, data, next, prev):
+        self.data = data #Keeps the value of the node
+        self.next = next #Stores pointer to next DNode object in DLinkedList
+        self.prev = prev #Stores pointer to previous DNode object in DLinkedList
+        
+
+
+
