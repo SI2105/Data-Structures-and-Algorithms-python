423612ca366168dada13c6628747c069a63381a7
Sakibul Islam <120340281+SI2105@users.noreply.github.com>
2023-12-29 22:23:26 +0000

    DLinkedList class initialised
    DLinkedList class is the class for the actual implementation of the Doubly Linked List. It simply contains two pointer to DNode objects, one for the Head and one for the Tail.
    It also stores the length of the doubly linked list in the length variable.

--- data-structures/doubly-linked-list.py
+++ data-structures/doubly-linked-list.py
@@ -1,9 +1,13 @@
 class DNode: #Doubly Linked List Node with the required variables.
     def __init__(self, data, next, prev):
-        self.data = data #Keeps the value of the node
-        self.next = next #Stores pointer to next DNode object in DLinkedList
-        self.prev = prev #Stores pointer to previous DNode object in DLinkedList
+        self.data = data # Keeps the value of the node
+        self.next = next # Stores pointer to next DNode object in DLinkedList
+        self.prev = prev # Stores pointer to previous DNode object in DLinkedList
         
 
-
+class DLinkedList: #Doubly Linked List class
+    def __init__(self):
+        self.head = None # Stores reference to the head DNode in the DLinkedList
+        self.tail = None # Stores reference to the tail DNode in the DLinkedList
+        self.length = 0  # Stores length of DLinkedList
 
