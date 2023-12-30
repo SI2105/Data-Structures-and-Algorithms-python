90e53498bff599065e1500169e2e4980060719be
Sakibul Islam <120340281+SI2105@users.noreply.github.com>
2023-12-30 15:55:59 +0000

    Doubly Linked List __str__ function created
    __str__ allows us to convert the doubly linked list into a string for printing.

--- data-structures/doubly_linked_list.py
+++ data-structures/doubly_linked_list.py
@@ -12,6 +12,23 @@ class DLinkedList: #Doubly Linked List class
         self.head = None # Stores reference to the head DNode in the DLinkedList
         self.tail = None # Stores reference to the tail DNode in the DLinkedList
         self.length = 0  # Stores length of DLinkedList
+
+    def __str__(self): # Prints the Doubly Linked List both reversed and
+        # normal to make sure the next and prev pointer are both correct
+        st = ""
+        ptr = self.head
+        while ptr != None:
+            st = st + str(ptr.data)
+            st = st+" -> "
+            ptr = ptr.next
+        st += "None, and reversed: "
+        ptr = self.tail
+        while ptr != None:
+            st = st + str(ptr.data)
+            st = st+" -> "
+            ptr = ptr.prev
+        return st+"None"
+
     def append(self, data):
         self.length += 1 #Increases the size of the DLinkedList
         if self.head == None: #Case for when DLinkedList is empty
@@ -21,3 +38,6 @@ class DLinkedList: #Doubly Linked List class
             new_node = DNode(data, None, self.tail) # New Node with the data, next pointer to none and prev pointer to tail is created.
             self.tail.next = new_node # Tails next pointer points to the new_node
             self.tail = new_node #the tail now points to new node
+
+
+
